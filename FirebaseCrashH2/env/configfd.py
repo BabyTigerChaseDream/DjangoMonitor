b987bc25758b7f146f25d5f81f5d9e4bf5ae0b39
Jia Guo <jia.guo@booking.com>
2021-08-12 19:35:41 +0800

    Dockerfile init

---
+++ FirebaseCrashH2/env/configfd.py
@@ -0,0 +1,12 @@
+# mkdir 
+'''
+sudo mkdir -p /var/run/secrets/booking.com/
+sudo mkdir -p /etc/bookings/pool_roster/
+'''
+
+# copy file
+'''
+cp ../secrets/booking.com/db.json /var/run/secrets/booking.com/ 
+cp ../pool_roster/experiencesdb-dqs_set /etc/bookings/pool_roster/
+cp ../pool_roster/androiddb-dqs_set /etc/bookings/pool_roster/
+'''
