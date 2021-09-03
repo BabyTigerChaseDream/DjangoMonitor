1d5c12c2e332b65faa0960bec028a9e78198bb63
Jia Guo <jia.guo@booking.com>
2021-09-03 12:37:31 +0800

    add 'get_issue'

--- FirebaseCrashH2/src/issues.py
+++ FirebaseCrashH2/src/issues.py
@@ -29,10 +29,10 @@ class Issue:
 		where issue_id={issue_id};
 	''' 
 	def __init__(self, issue_id, table='firebase_crashlytics_com_booking_ANDROID', DBEngine=None):
-		DBEngine = DB().DBEngine
+		self.DBEngine = DB().DBEngine
 		# sql to get data per request
 		self.table = table
-		self.issue_id = issue_id
+		self.issue_id = str(issue_id)
 		
 		self.exceptions = None 
 		self.frames = None 
@@ -42,10 +42,13 @@ class Issue:
 
 		self.sql_cmd = Issue.RETRIEVE_ISSUE_CONTENT_BY_ISSUE_ID.format(
 				table = table,
-				issue_id = issue_id
+				issue_id = str(issue_id)
 			)
 
-		self.cursor = DBEngine.execute(self.sql_cmd)
+	def get_issue(self,sql_cmd=None):
+		if not sql_cmd:
+			sql_cmd = self.sql_cmd
+		self.cursor = self.DBEngine.execute(sql_cmd)
 		return self.cursor
 	
 	def get_issue_exceptions(self, exception_key='exceptions', issue_id_key='issue_id')->dict:
