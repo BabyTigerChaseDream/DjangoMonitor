572a78a73b5a29575d679c22009587bbca41367c
Jia Guo <jia.guo@booking.com>
2021-10-03 22:48:15 +0800

    Reform all user configuration in 'cards'

--- FirebaseCrashH2/apps/userconfig/filters.py
+++ FirebaseCrashH2/apps/userconfig/filters.py
@@ -1,12 +1,17 @@
 from django.forms.fields import ChoiceField
 from .models import Config, PLATFORM_CHOICES
 import django_filters
+from django import forms
+
+
 
 class UserConfigFilter(django_filters.FilterSet):
 	# django-filter embed expr
-	team = django_filters.CharFilter(lookup_expr='icontains')
-	slack_channel = django_filters.CharFilter(lookup_expr='icontains') 
-	email_address = django_filters.CharFilter(lookup_expr='icontains')
+	team = django_filters.CharFilter(lookup_expr='icontains', label='Team', widget=forms.TextInput(attrs={'class': 'input-group input-group-sm'}))
+	platform = django_filters.ChoiceFilter(choices=PLATFORM_CHOICES, label='Platform') 
+
+	slack_channel = django_filters.CharFilter(lookup_expr='icontains', label='Slack', widget=forms.TextInput(attrs={'class': 'input-group input-group-sm'})) 
+	email_address = django_filters.CharFilter(lookup_expr='icontains', label='Email', widget=forms.TextInput(attrs={'class': 'input-group input-group-sm'}))
 	# django-filter customized filter
 	#https://stackoverflow.com/questions/57668670/how-to-use-same-same-django-filters-charfilter-field-for-two-separate-fields
 
@@ -20,12 +25,11 @@ class UserConfigFilter(django_filters.FilterSet):
     release_year__gt = django_filters.NumberFilter(field_name='release_date', lookup_expr='year__gt')
     release_year__lt = django_filters.NumberFilter(field_name='release_date', lookup_expr='year__lt')
 	'''
-	crash___gt = django_filters.NumberFilter(field_name='crash_count', lookup_expr='gt')
+	crash___gt = django_filters.NumberFilter(field_name='crash_count', lookup_expr='gt', label='Mininum Crash Count')
 	#crash___lt = django_filters.NumberFilter(field_name='crash_count', lookup_expr='lt')
-	user___gt = django_filters.NumberFilter(field_name='total_user', lookup_expr='gt')
+	user___gt = django_filters.NumberFilter(field_name='total_user', lookup_expr='gt', label='Mininum User Affect')
 	#user___lt = django_filters.NumberFilter(field_name='total_user', lookup_expr='lt')
 
-	platform = django_filters.ChoiceFilter(choices=PLATFORM_CHOICES) 
-	files = django_filters.CharFilter(lookup_expr='icontains') 
-	keywords = django_filters.CharFilter(lookup_expr='icontains')
-	tags = django_filters.CharFilter(lookup_expr='icontains')
+	files = django_filters.CharFilter(lookup_expr='icontains', label='File Names', widget=forms.TextInput(attrs={'class': 'input-group input-group-sm'})) 
+	keywords = django_filters.CharFilter(lookup_expr='icontains', label='Keywords in Logs', widget=forms.TextInput(attrs={'class': 'input-group input-group-sm'}))
+	#tags = django_filters.CharFilter(lookup_expr='icontains')
