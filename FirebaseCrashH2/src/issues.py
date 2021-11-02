0cb1a8582468f464773096df60e58086e8b7a33a
Jia Guo <jia.guo@booking.com>
2021-11-02 15:43:16 +0800

    optimized 'RETRIEVE_STACKTRACES_BY_ISSUE_ID' speed up access and just got latest stacktraces

--- FirebaseCrashH2/src/issues.py
+++ FirebaseCrashH2/src/issues.py
@@ -44,7 +44,7 @@ class Issue:
 		select 
 			stacktraces 
 		from `{stacktrace_table}` 
-		where issue_id='{issue_id}' limit 3;
+		where issue_id='{issue_id}' order by insert_id desc limit 1;
 	'''
 
 	stacktrace_table = 'firebase_crashlytics_stacktraces'
